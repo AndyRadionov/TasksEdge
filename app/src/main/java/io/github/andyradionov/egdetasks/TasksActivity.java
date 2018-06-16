90e9202e0792cf04ccb963bee45a37859fb9d688
Andrey Radionov <andyomsk@gmail.com>
2018-06-16 15:25:18 +0300

    Menu Added

--- app/src/main/java/io/github/andyradionov/egdetasks/TasksActivity.java
+++ app/src/main/java/io/github/andyradionov/egdetasks/TasksActivity.java
@@ -1,7 +1,11 @@
 package io.github.andyradionov.egdetasks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class TasksActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class TasksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
